14a238c44876136c76130721c0f31bcafab797ed
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 11:54:49 +0100

    exo8

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $addition = 3 + 4;
+    $multiplication = 5 * 20;
+    $division = 45 / 5;
+    echo "$addition $multiplication $division";
+    ?>
+  </body>
